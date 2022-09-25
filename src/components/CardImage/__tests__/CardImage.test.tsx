46e73e053039a91c911a3779ce63bab95663607c
thomasbalmant <thomas_dru@hotmail.com>
2022-09-25 10:53:56 -0300

    cardImage test

---
+++ src/components/CardImage/__tests__/CardImage.test.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import { CardImage } from '../CardImage';
+
+describe('StarRating', () => {
+   it('Element', () => {})
+})
